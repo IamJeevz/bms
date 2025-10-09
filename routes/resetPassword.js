11e631bce6f43db21aed520ddedc087205622694
IamJeevz <jeevansaju1@outlook.com>
2025-10-09 21:38:38 +0530

    forgot password

--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -37,4 +37,32 @@ router.post('/reset-password', async (req, res) => {
   }
 });
 
+router.post('/reset-frg_password', async (req, res) => {
+  const { phone, newPassword} = req.body;
+
+  if (!phone || !newPassword) {
+    return res.status(400).json({ success: false, message: 'Missing required fields' });
+  }
+
+  try {
+    // ✅ Check if user exists
+    const [users] = await db.query('SELECT * FROM sec_user_mst WHERE phone_number = ?', [phone]);
+    if (users.length === 0) {
+      return res.status(400).json({ success: false, message: 'User not found' });
+    }
+
+
+    // ✅ Hash new password
+    const hashedNew = await bcrypt.hash(newPassword, 10);
+
+    // ✅ Update password in DB
+    await db.query('UPDATE sec_user_mst SET password = ? WHERE phone_number = ?', [hashedNew, phone]);
+
+    return res.json({ success: true, message: 'Password reset successfully' });
+  } catch (err) {
+    console.error('Reset password error:', err);
+    return res.status(500).json({ success: false, message: 'Server error' });
+  }
+});
+
 module.exports = router;
